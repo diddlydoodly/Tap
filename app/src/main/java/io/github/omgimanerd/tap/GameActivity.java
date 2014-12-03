310124779a0473f21073c3f6915829bb8f108e82
Alvin Lin <alvin.lin@stuypulse.com>
2014-12-03 10:44:57 -0500

    base files

--- app/src/main/java/io/github/omgimanerd/tap/GameActivity.java
+++ app/src/main/java/io/github/omgimanerd/tap/GameActivity.java
@@ -1,36 +1,36 @@
 package io.github.omgimanerd.tap;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class GameActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-    }
+  public static final String TAG = GameActivity.class.getSimpleName();
 
+  private Button startButton;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.game, menu);
-        return true;
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    startButton = (Button) findViewById(R.id.startButton);
+    startButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        startGame();
+      }
+    });
+  }
+
+  private void startGame() {
+    Intent intent = new Intent(this, GameActivity.class);
+    startActivity(intent);
+  }
 }
---
+++ app/src/main/java/io/github/omgimanerd/tap/game/Ball.java
@@ -0,0 +1,15 @@
+package io.github.omgimanerd.tap.game;
+
+/**
+ * Created by omgimanerd on 12/1/14.
+ */
+public class Ball {
+  private final int amplitude = 10;
+  private int x;
+  private int y;
+
+  public Ball() {
+    this.x = 0;
+    this.y = 0;
+  }
+}
